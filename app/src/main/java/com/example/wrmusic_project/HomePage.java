3cac84f556581d070e0913196a5353ef8ff11229
Grisella <grisella.zega@binus.ac.id>
2024-08-01 16:22:04 +0700

    Initial Project

--- app/src/main/java/com/example/wrmusic_project/HomePage.java
+++ app/src/main/java/com/example/wrmusic_project/HomePage.java
@@ -1,16 +1,28 @@
 package com.example.wrmusic_project;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+import com.example.wrmusic_project.R;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Locale;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView welcomeUser;
     private TextView username;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +33,43 @@ public class HomePage extends AppCompatActivity {
 
         String username = getIntent().getStringExtra("USERNAME_KEY");
 
-        if (username != null){
-            welcomeUser.setText("Welcome\n" + username.toUpperCase(Locale.ROOT) + "!");
-        }
-        else {
-            welcomeUser.setText("Welcome!");
+        if (username != null) {
+            welcomeUser.setText(username.toUpperCase(Locale.ROOT) + "!");
+        } else {
+            welcomeUser.setText("USERNAME!");
         }
 
+        drawerLayout = findViewById(R.id.drawerMenu);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+        }
+        return true;
     }
 }
